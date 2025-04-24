99250077dab28c0c9bdddc2cfe4f8d54a8c19d4b
terrytraylor <terry.traylor@ndsu.edu>
2025-04-24 11:31:45 -0500

    Assignment_6 with committed second activity protections in place.

--- app/src/main/java/com/example/assignment_two/MainActivity.kt
+++ app/src/main/java/com/example/assignment_two/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.example.assignment_two
 
-import android.content.ActivityNotFoundException
 import android.content.Intent
-import android.net.Uri
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.provider.MediaStore
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    //Variables Required to Execute Requirement 3
+    private val REQUEST_ASSIGNMENT5_PERMISSION = 205
+    private val CUSTOM_PERMISSION = "com.Assignment_Two.MSE412"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,6 +26,23 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //Launch the activity code
+        // Start the second (protected and exported) activity
+        fun launchSecondActivity() {
+            val intent = Intent("com.secondactivity")
+            intent.setPackage("com.example.assignment_two") // Make sure it targets your own app
+            startActivity(intent)
+        }
+
+        //Requirement 3:  Request Permission from the main activity
+        // Request the custom dangerous permission at startup
+        if (ContextCompat.checkSelfPermission(this, CUSTOM_PERMISSION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(CUSTOM_PERMISSION), REQUEST_ASSIGNMENT5_PERMISSION)
+        } else {
+            launchSecondActivity()
+        }
+
         val buttonClick = findViewById<Button>(R.id.btnExplicit)
         buttonClick.setOnClickListener {
             val intent = Intent(/* packageContext = */ this, /* cls = */ SecondActivity::class.java)
